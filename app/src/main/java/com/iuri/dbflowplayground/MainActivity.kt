35d462ccb5ddb24ca31a4b423bf67ba0ea890421
Iuri Cernov <iuri.cernov@gmail.com>
2020-02-12 16:19:24 -0300

    Removed options menu

--- app/src/main/java/com/iuri/dbflowplayground/MainActivity.kt
+++ app/src/main/java/com/iuri/dbflowplayground/MainActivity.kt
@@ -2,11 +2,7 @@ package com.iuri.dbflowplayground
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,22 +14,6 @@ class MainActivity : AppCompatActivity() {
 
         fab.setOnClickListener {
             startActivityForResult(Intent(this, AddSomeoneActivity::class.java), 99)
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
         }
     }
 }
